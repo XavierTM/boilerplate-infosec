5991418fc4bea2e3d3f1303232e0305f303efdc8
Xavier Mukodi <xavier@xavier.local>
2022-09-13 13:00:11 +0200

    suppressed x-powered-by header

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const app = express();
+
 
 require('dotenv').config();
+
+const express = require('express');
+const helmet = require('helmet');
+const app = express();
+
 
 
 
@@ -50,13 +54,24 @@ require('dotenv').config();
 
 
 module.exports = app;
+
+
+// middlewares
+
 const api = require('./server.js');
+
+app.use(helmet.hidePoweredBy());
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
+
+
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
